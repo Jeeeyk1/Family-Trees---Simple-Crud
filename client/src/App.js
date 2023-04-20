d6d31a2aa90a8a2455db1a03fd0b023a98b892a5
Jeeyk1 <nooojake@yahoo.com>
2023-04-20 14:11:16 +0800

    pushing all

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import DeleteFamilyMember from "./components/DeleteFamilyMember";
 import Display from "./components/Display";
 import FamilyMembers from "./components/FamilyMembers";
 import Header from "./components/Header";
+import UpdateFamilyMember from "./components/UpdateFamilyMember";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <br />
       <h1>Add and Delete Family Member</h1>
       <DeleteFamilyMember />
+      <br />
+      <UpdateFamilyMember />
     </div>
   );
 }
